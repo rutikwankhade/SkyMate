4be561757aa7b30974e93757d2d2444e6b0feef7
rutik wankhade <rutikwankhade2@gmail.com>
2020-06-02 14:11:12 +0530

    stage 1 : user interface

--- app.js
+++ app.js
@@ -2,7 +2,10 @@
 let input=document.querySelector('.city-input');
 
 let btn=document.querySelector('.btn-search');
-btn.addEventListener('click', displayWeather)
+btn.addEventListener('click', ()=>{
+    displayWeather();
+    // updateUI();
+})
 
 
 
@@ -21,8 +24,44 @@ btn.addEventListener('click', displayWeather)
         return data;
 })
     .then(data=>{
-        
         console.log(data);
+        let temp=data.list[0].main.feels_like;
+        let weather=data.list[0].weather[0].description;
+        let windSpeed=data.list[0].wind.speed;
+        let maxTemp=data.list[0].main.temp_max;
+        let minTemp=data.list[0].main.temp_min;
+        let humidity=data.list[0].main.humidity;
+        let pressure=data.list[0].main.pressure;
+
+
+        tempText.textContent=temp;
+        weatherText.textContent=weather;       
+        windspeedText.textContent=windSpeed;
+        maxTempText.textContent=maxTemp;
+
+        minTempText.textContent=minTemp;
+        humidityText.textContent=humidity;
+        pressureText.textContent=pressure;
+
+
+
     })
     .catch(error=>console.log(error))
 }
+
+let tempText = document.querySelector('.temp');
+
+let weatherText = document.querySelector('.weather');
+let windspeedText = document.querySelector('.wind-speed');
+let maxTempText = document.querySelector('.max-temp');
+let minTempText = document.querySelector('.min-temp');
+let humidityText = document.querySelector('.humidity');
+let pressureText = document.querySelector('.pressure');
+
+
+
+
+
+
+// function updateUI(weather){
+// }
