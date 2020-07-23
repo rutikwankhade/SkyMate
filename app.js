f6007ca2500a2740e9c60294037905f115dc59fe
rutik wankhade <rutikwankhade2@gmail.com>
2020-07-23 08:34:44 +0530

    added spinner

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 let input=document.querySelector('.city-input');
-
+let weatherData = document.querySelector('.weather-data');
 let btn=document.querySelector('.btn-search');
 btn.addEventListener('click', ()=>{
     displayWeather();
@@ -13,13 +13,35 @@ document.addEventListener('keypress',(event)=>{
     }
 })
 
+const renderLoader = () => {
+    let loader = `
+    <div class="d-flex justify-content-center m-2">
+      <div class="spinner-border spinner " role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+      </div>
+      `;
+    weatherData.insertAdjacentHTML('beforebegin', loader);
+}
+
+const clearLoader = () => {
+    let spinner = document.querySelector('.spinner');
+    if (spinner) {
+        spinner.parentElement.removeChild(spinner);
+        
+
+    }
+
+}
+
+
 function displayWeather(){
     let place = input.value;
     console.log(place);
 
     let api="https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/find?q="+ place +"&units=metric&appid=850cac4c51c8a61b959dfe0593e8cb07";
     console.log(api);
-
+    renderLoader();
     fetch(api)
     .then(response=>{
         data=response.json();
@@ -27,6 +49,7 @@ function displayWeather(){
 })
     .then(data=>{
         console.log(data);
+        clearLoader();
      //access data from the fetched object 
         let temp=data.list[0].main.feels_like;
         let weather=data.list[0].weather[0].description;
@@ -59,11 +82,30 @@ function displayWeather(){
         weatherIcon.setAttribute('src','icons/overcast.png');
 
 
+
+
     })
     .catch(error=>{
         console.log(error);
         alert('please enter a valid place.');
 })
+
+
+//daily forecast
+
+let forecastapi="https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/forecast/daily/find?q="+ place +"&units=metric&appid=4b2ab8f040bb8dd2f8d80e58c3e29cb3";
+fetch(forecastapi)
+.then(response=>{
+    forecastdata=response.json();
+    return forecastdata;
+})
+.then(forecastdata=>{
+    console.log(forecastdata);})
+
+
+
+
+
 }
 let weatherIcon=document.querySelector('.weather-icon');
 
@@ -73,4 +115,7 @@ let today=new Date();
 const options={year: 'numeric', month: 'long', day: 'numeric' }
 date.textContent=today.toLocaleDateString('en-US',options);
 
+
+
+//daily forcast
 
